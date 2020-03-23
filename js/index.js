83111e6ee35bb65dd364bd607155dbac344ec298
StepanAndrosov <60291731+StepanAndrosov@users.noreply.github.com>
2020-03-23 19:22:15 +0300

    .input-number  display: flex;

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-var link = document.querySelector(".search-button");
+const link = document.querySelector(".search-button");
 
-var popup = document.querySelector(".appointment-form");
+const popup = document.querySelector(".appointment-form");
 
  link.addEventListener("click", function (evt) {
     evt.preventDefault();
-    popup.classList.toggle("modal-window");
+    popup.classList.toggle("hidden");
   });
